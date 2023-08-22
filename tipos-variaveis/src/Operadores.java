38d551ad413eb553961388dc658e566ae6cede53
Willames Vital <willames.vital@liferay.com>
2023-08-22 16:07:46 -0300

    Adicionar novo projeto java SmartTv

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,22 @@
+import javax.swing.plaf.synth.SynthStyleFactory;
+
+public class Operadores {
+    
+    public static void main(String[]args ){
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if(condicao1 && ( 7 > 4)){
+            System.out.println("As duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições são verdadeiras");
+        }
+
+        System.out.println("Fim");
+    }
+
+
+}
